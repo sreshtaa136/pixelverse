b12387133729ef89295dfbe2ee49456c15290306
sreshtaa136 <sreshtaa.t@gmail.com>
2024-12-02 22:54:43 +0530

    add trending videos

--- components/Trending.jsx
+++ components/Trending.jsx
@@ -1,17 +1,81 @@
-import { View, Text } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  ImageBackground,
+} from "react-native";
+import React, { useState } from "react";
 import { FlatList } from "react-native-gesture-handler";
+import * as Animatable from "react-native-animatable";
+import { icons } from "@/constants";
 
 const Trending = ({ posts }) => {
+  const [activeItem, setActiveItem] = useState(posts[0]);
+
   return (
     <FlatList
       data={posts}
       keyExtractor={(item) => item.$id}
       horizontal
       renderItem={({ item }) => (
-        <Text className="text-white text-xl">{item.$id}</Text>
+        <TrendingItem activeItem={activeItem} item={item} />
       )}
     />
+  );
+};
+
+const zoomIn = {
+  0: {
+    scale: 0.9,
+  },
+  1: {
+    scale: 1,
+  },
+};
+
+const zoomOut = {
+  0: {
+    scale: 1,
+  },
+  1: {
+    scale: 0.9,
+  },
+};
+
+const TrendingItem = ({ activeItem, item }) => {
+  const [play, setPlay] = useState(false);
+
+  console.log("activeItem", activeItem);
+  return (
+    <Animatable.View
+      className="mr-5"
+      animation={activeItem.$id === item.$id ? zoomIn : zoomOut}
+      duration={500}
+    >
+      {play ? (
+        <Text className="text-white">playing</Text>
+      ) : (
+        <TouchableOpacity
+          activeOpacity={0.7}
+          onPress={() => setPlay(true)}
+          className="relative flex justify-center items-center"
+        >
+          <ImageBackground
+            source={{
+              uri: item.thumbnail,
+            }}
+            className="w-52 h-72 rounded-[33px] my-5 overflow-hidden shadow-lg shadow-black/40"
+            resizeMode="cover"
+          />
+          <Image
+            source={icons.play}
+            className="w-12 h-12 absolute"
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
+      )}
+    </Animatable.View>
   );
 };
 
